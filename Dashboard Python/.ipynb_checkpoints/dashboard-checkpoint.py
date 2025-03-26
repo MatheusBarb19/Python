11186d4fe3a783271f309778fcc65e3ffe0427d3
Matheus Barbosa <145235787+MatheusBarb19@users.noreply.github.com>
2025-03-26 00:05:42 -0300

    Delete Dashboard Python/.ipynb_checkpoints directory

--- Dashboard Python/.ipynb_checkpoints/dashboard-checkpoint.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import plotly.express as px
-import streamlit as st
-
-#Lendo df com Pandas
-df = pd.read_csv(r"C:\Users\mathe\Documents\GitHub\Python\Dashboard Python\Posio ocupada pelos 100 maiores municpios em relao ao Produto Interno Bruto - 2020.csv", sep=",")
-
